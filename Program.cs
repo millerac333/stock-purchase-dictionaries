301cbbdc6912454668e2b12f93f608e469add779
Aaron Miller <millerac333@gmail.com>
2018-08-22 15:29:09 -0500

    added stocks in main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dictionaries
 {
@@ -6,7 +7,12 @@ namespace dictionaries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> stocks = new Dictionary<string, string>();
+            stocks.Add("GM", "General Motors");
+            stocks.Add("CAT", "Caterpillar");
+            stocks.Add("TSLA", "Tesla");
+            stocks.Add("GOOG", "Google");
+            stocks.Add("NKE", "Nike");
         }
     }
 }
